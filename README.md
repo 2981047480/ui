b36f21856fd963d7bf2a07a697cb9382f941bb41
zhaoshihao <zhaoshihao@sensorsdata.com>
2024-07-24 13:47:57 +0800

    修改了一些样式 今天学会覆盖框架的样式了

--- README.md
+++ README.md
@@ -1,35 +1,4 @@
-# ui
-
-This template should help get you started developing with Vue 3 in Vite.
-
-## Recommended IDE Setup
-
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vitejs.dev/config/).
-
-## Project Setup
-
-```sh
-npm install
-```
-
-### Compile and Hot-Reload for Development
-
-```sh
-npm run dev
-```
-
-### Compile and Minify for Production
-
-```sh
-npm run build
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-npm run lint
-```
+# 7.24
+修改为如下样式：
+![7.24](./png/7_24.png)
+不知道为什么垂直居中一直生效不了 还得再看看
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,29 +1,33 @@
 <template>
-    <a-form class="warpper" :model="form" :style="{width: '400px'}" @submit="ClickHandler">
-        <div class="title">登录</div>
-        <a-form-item field="username" label="用户名">
-            <a-input v-model="form.username" placeholder="请输入用户名">
-                <template #prefix>
-                    <icon-user />
-                </template>
-            </a-input>
-        </a-form-item>
-        <a-form-item field="password" label="密码">
-            <a-input-password v-model="form.password" placeholder="请输入密码">
-                <template #prefix>
-                    <icon-lock />
-                </template>
-            </a-input-password>
-        </a-form-item>
-        <a-form-item field="is_remember">
-            <a-checkbox v-model="form.is_remember">
-                记住我
-            </a-checkbox>
-        </a-form-item>
-        <a-button type="primary" html-type="submit" :loading="isLoading" @click="LoadingHandler">
-            submit
-        </a-button>
-    </a-form>
+    <div class="login-component-wapper">
+        <a-form class="login-component" :model="form" @submit="ClickHandler">
+            <div class="title">登录</div>
+            <div>
+                <a-form-item field="username" label="用户名">
+                    <a-input v-model="form.username" placeholder="请输入用户名">
+                        <template #prefix>
+                            <icon-user />
+                        </template>
+                    </a-input>
+                </a-form-item>
+                <a-form-item field="password" label="密码">
+                    <a-input-password v-model="form.password" placeholder="请输入密码">
+                        <template #prefix>
+                            <icon-lock />
+                        </template>
+                    </a-input-password>
+                </a-form-item>
+                <a-form-item field="is_remember">
+                    <a-checkbox v-model="form.is_remember">
+                        记住我
+                    </a-checkbox>
+                </a-form-item>
+            </div>
+            <a-button class="submit-button" type="primary" html-type="submit" :loading="isLoading" @click="LoadingHandler" auto-label-width>
+                submit
+            </a-button>
+        </a-form>
+    </div>
 </template>
 
 <script setup>
@@ -47,7 +51,7 @@
     }
 </script>
 
-<style>
+<style scoped>
     .title{
         font-size: 30px;
         color: var(--color-neutral-8);
@@ -55,7 +59,39 @@
         margin-bottom: 25px;
     }
 
-    .warpper{
+    .login-component-wapper{
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.594);
+    }
+    
+    .login-component{
+        height: 300px;
+        width: 500px;
         margin: auto;
+        background-color: #FFF;
+    }
+   
+    .arco-btn-primary, .arco-btn-primary[type="button"], .arco-btn-primary[type="submit"] {
+        color: #fff;
+        background-color: rgb(165, 18, 165);
+        height: 40px;
+        border: 1px solid transparent;
+        margin: 3px;
+    }
+    
+    .arco-input-wrapper .arco-input {
+        width: 100%;
+        padding-right: 10;
+        padding-left: 0;
+        color: inherit;
+        line-height: 1.5715;
+        background: none;
+        border: none;
+        border-radius: 0;
+        outline: none;
+        cursor: inherit;
+        -webkit-appearance: none;
+        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
     }
 </style>
